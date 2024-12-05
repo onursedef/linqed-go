f2b7f2754e32d5e359264afc0d7cb4676e441dd5
onursedef <onur.sedef@ottoo.com>
2024-12-05 15:50:08 +0300

    feat: add go doc

---
+++ doc.go
@@ -0,0 +1,6 @@
+/*
+Package linq_go is a simple and powerful package for working with collections in Go.
+linq_go is inspired by the .NET Language Integrated Query (LINQ) library.
+Author: Onur Sedef
+*/
+package linqed_go // import "github.com/onursedef/linqed-go"
