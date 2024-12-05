0a7fe9a4288ff0bec791c62fd2e542f1f9d0661e
onursedef <onur.sedef@ottoo.com>
2024-12-05 18:31:54 +0300

    feat: select returns given type

--- transform.go
+++ transform.go
@@ -1,7 +1,10 @@
 package linqed_go
 
 // Select projects each element of the collection into a new form
-func (q *Queryable[T]) Select(projection func(T) any) *Queryable[T] {
-	q.projection = projection
-	return q
+func Select[T, R any](q *Queryable[T], projection func(T) R) *Queryable[R] {
+	var result []R
+	for _, item := range q.items {
+		result = append(result, projection(item))
+	}
+	return From(result)
 }
